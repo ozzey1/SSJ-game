d5dd580383278fb3d42f5845163093c9fe90d9a5
ArcyFox <foxarcy@gmail.com>
2020-03-27 22:14:14 +0000

    Turtle Movement Code

---
+++ SSJ/Assets/Scripts/FrogBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogBehaviour : MonoBehaviour
+{
+    void Start()
+    {}
+
+    void FixedUpdate()
+    {}
+}
