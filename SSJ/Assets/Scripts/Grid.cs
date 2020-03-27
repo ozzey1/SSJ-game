da53629e5620888a4a90a8d5515ab46680a9d52a
Luca Ballarati <1uca8allarati@gmail.com>
2020-03-28 00:17:00 +0100

    nothing

---
+++ SSJ/Assets/Scripts/Grid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    private int height;
+    private int width;
+
+    private int[,] grid = { 
+        { 0, 1, 0, 0, -1, -1, -1 }, 
+        { 0, 1, 0, 0, 0, 1, 0 }, 
+        { 0, 1, 0, 0, 0, 1, 0 }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //grid = { { 0, 1, 0, 0, -1, -1, -1},{ 0, 1, 0, 0, 0, 1, 0},{ 0, 1, 0, 0, 0, 1, 0} };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SSJ/Assets/Scripts/tile_info.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile_info : MonoBehaviour
+{
+    public int type;
+    public bool passable;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
