b763c501c6a07dfb257d23d4f3567ab6aa28dfa6
Luca Ballarati <1uca8allarati@gmail.com>
2020-03-28 12:38:58 +0100

    pushing ozzey's build with my additions
    I implemented game over and combination of characters, as well as prefìventing movement with keys held down

---
+++ SSJ ozzey's build/Assets/Scripts/GridMove.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMove : MonoBehaviour
+{
+    public float moveSpeed = 2;
+    public Transform moveTarget; //Empty sprite moves towards
+    public LayerMask whatStopsMovement; //holds colliders for boundaries
+    public LayerMask hazards; //holds colliders for tiles that trigger gameover
+    public LayerMask characters; //holds colliders for other characters
+
+    public GameObject MainManager;
+
+    private bool moved;
+
+    private MainManager MM;
+
+    void Start()
+    {
+        moveTarget.parent = null;
+        moved = false;
+        MM = MainManager.GetComponent<MainManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move to moveTarget
+        transform.position = Vector3.MoveTowards(transform.position, moveTarget.position, moveSpeed * Time.deltaTime);
+        //sto
+        if (Vector3.Distance(transform.position, moveTarget.position) <= 0.5f)
+        {
+            //gets horizontal
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1 && !moved)
+            {
+                moved = true;
+
+                //triggers game over if there is a hazard ahead
+                if (Physics2D.OverlapCircle(moveTarget.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, hazards))
+                {
+                    MM.Defeat();
+                }
+
+                //detects contact with other characters
+                if (Physics2D.OverlapCircle(moveTarget.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, characters))
+                {
+
+                }
+
+                //moves if no collisions ahead
+                if (!Physics2D.OverlapCircle(moveTarget.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
+                {
+                    //moves to moveTarget
+                    moveTarget.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    //okami's rotate/flip sprite script
+                    if (Mathf.Sign(Input.GetAxisRaw("Horizontal")) == 1) { transform.eulerAngles = new Vector3(0, 0, -90); }
+                    else { transform.eulerAngles = new Vector3(0, 0, 90); }
+                }
+            }
+
+            //gets vertical
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1 && !moved)
+            {
+                moved = true;
+
+                //triggers game over if there is a hazard ahead
+                if (Physics2D.OverlapCircle(moveTarget.position + new Vector3(Input.GetAxisRaw("Vertical"), 0f, 0f), .2f, hazards))
+                {
+                    MM.Defeat();
+                }
+
+                //detects contact with other characters
+                if (Physics2D.OverlapCircle(moveTarget.position + new Vector3(Input.GetAxisRaw("Vertical"), 0f, 0f), .2f, characters))
+                {
+
+                }
+
+                //moves if no collisions ahead
+                if (!Physics2D.OverlapCircle(moveTarget.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
+                {
+                    //moves to moveTarget
+                    moveTarget.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    //okami's rotate/flip sprite script
+                    if (Mathf.Sign(Input.GetAxisRaw("Vertical")) == 1) { transform.eulerAngles = new Vector3(0, 0, 0); }
+                    else { transform.eulerAngles = new Vector3(0, 0, 180); }
+                }
+            }
+
+            else moved = false;
+        }
+    }
+}
---
+++ SSJ ozzey's build/Assets/Scripts/MainManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public string[] charNames;
+
+    public GameObject[] characters;
+
+    public void Defeat()
+    {
+        GameOver();
+    }
+
+    public void CheckVictory() { AllTogether(); }
+
+    public int charNameToIndex(string name)
+    {
+        int index = -1;
+        for (int i = 0; i < charNames.Length; i++) if (charNames[i] == name) index = i;
+        return index;
+    }
+    public bool Combine(GameObject charA, GameObject charB)
+    {
+        int a = charNameToIndex(charA.name);
+        int b = charNameToIndex(charB.name);
+
+        if (a * b < 0) return false;
+        if (combineMatrix[a, b] < 0) return false;
+
+        Vector2 pos = charA.transform.position;
+        Quaternion rot = charA.transform.rotation;
+        Destroy(charA);
+        Destroy(charB);
+        Instantiate(characters[combineMatrix[a, b]], pos, rot);
+
+        return true;
+    }
+
+    private int[,] combineMatrix =
+    {
+        {-1,2},
+        {2,-1}
+    };
+
+    private void GameOver() { Debug.Log("Game Over"); }
+
+    private void Victory() { Debug.Log("You Win!!!"); }
+
+    private bool AllTogether()
+    {
+        Vector3 coordinates = Vector3.zero;
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (i == 0) coordinates = characters[i].transform.position;
+            else if (coordinates != characters[i].transform.position) return false;
+        }
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
